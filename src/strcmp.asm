2a23e28fb6c6adba839d52ad89ba35753cb3328b
Tom TRIQUET <triquet.tom@epitech.eu>
2020-02-13 22:56:08 +0100

    [MODIF] Makefile, adding strcmp to compilation [ADD] strcmp.asm, working but not every case handled

---
+++ src/strcmp.asm
@@ -0,0 +1,26 @@
+section .text
+    global my_strcmp:function
+    ; int strcmp(const char *first, const char *second);
+
+my_strcmp:
+        ; rdi = first
+        ; rsi = second
+
+    xor cl, cl
+    xor dl, dl
+.start:
+    cmp BYTE [rdi], 0
+    jz .end
+    cmp BYTE [rsi], 0
+    jz .end
+
+    add dl, BYTE [rdi]
+    add cl, BYTE [rsi]
+    inc rdi
+    inc rsi
+    jmp .start
+
+.end:
+    sub dl, cl
+    movsx rax, dl
+    ret
