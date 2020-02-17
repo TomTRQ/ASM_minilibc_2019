7be4048fc6df003a4f96b44dec9f1d922f1aba2e
Tom TRIQUET <triquet.tom@epitech.eu>
2020-02-17 10:08:32 +0100

    [MODIF] memmove.asm

--- src/memmove.asm
+++ src/memmove.asm
@@ -5,7 +5,11 @@ section .text
     ; void *memmove(void *destination, const void *source, size_t size);
 
 my_memmove:
+        ;rdi = destination
+        ;rsi = source
+        ;edx = value
 
 .start:
 
 .end:
+    ret
