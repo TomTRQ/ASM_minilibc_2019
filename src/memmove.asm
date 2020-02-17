29b39ca94c7f56806a222db149cdca5f926e8471
Tom TRIQUET <triquet.tom@epitech.eu>
2020-02-17 16:17:58 +0100

    [MODIF] memmove.asm, cpy memcpy code into memmove

--- src/memmove.asm
+++ src/memmove.asm
@@ -1,18 +1,28 @@
 BITS 64
 
 section .text
-    global my_memmove:function
+    global memmove:function
     ; void *memmove(void *destination, const void *source, size_t size);
 
-my_memmove:
-        ;rdi = destination
-        ;rsi = source
-        ;edx = value
+memmove:
+    ; rdi = pointer
+    ; rsi = value
+    ; rdx = count
 
-        enter 0, 0
+    enter 0, 0
+    xor rcx, rcx                                ;On initialise une valeur à 0
+    xor bh, bh                                  ;On initialise un registre tampon
 
 .start:
+    cmp rcx, rdx                                ;On compare rcx avec le count
+    jz .end                                     ;Si égal on va à la fin du programme
+
+    mov bh, BYTE [rsi + rcx]                    ;Sinon on met la valeur de l'index actuel
+    mov BYTE [rdi + rcx], bh                    ;On ajoute le caractère à rdi
+    inc rcx                                     ;On incrémente la valeur
+    jmp .start                                  ;On repart au début
 
 .end:
+    mov rax, rdi                                ;On déplace notre registre dans rax pour le renvoyer
     leave
-    ret
+    ret                                         ;On quitte le programme en renvoyant rax
